116b57b7f035b02eda182e11c14802b050d82c34
Peskobar <150852999+Peskobar@users.noreply.github.com>
2025-08-25 17:06:55 +0200

    Uaktualnienie zależności i wsparcia wersji Pythona

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name="shodan",
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
+    python_requires=">=3.8",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+    ],
+)
